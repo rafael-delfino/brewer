7e25a7e41dd4b194b143c901f596fffa1cb175ff
Rafael Pereira Delfino <rafaelp.delfino@gmail.com>
2017-05-24 20:12:16 -0300

    Cadastro de Cerveja
    Criação da tela de cadastro de cerveja

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,8 +1,13 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -10,15 +15,24 @@ import com.algaworks.brewer.model.Cerveja;
 public class CervejasController {
 
 	@RequestMapping("/cervejas/novo")
-	public String novo() {
+	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(Cerveja cerveja) {
-		System.out.println("sku:" + cerveja.getSku());
-		System.out.println("nome:" + cerveja.getNome());
-		return "cerveja/CadastroCerveja";
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			return novo(cerveja);
+		}
+		
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
+		return "redirect:/cervejas/novo";
+	}
+	
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
 	}
 
 }
