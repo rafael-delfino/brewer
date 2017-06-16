67651ce29d010a2de2ca4e899163923f3ca3ee02
Rafael Pereira Delfino <rafaelp.delfino@gmail.com>
2017-06-16 15:09:10 -0300

    Gravação do nome da foto

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -75,11 +75,32 @@ public class Cerveja {
 	@JoinColumn(name = "codigo_estilo")
 	private Estilo estilo;
 	
+	private String foto;
+	
+	@Column(name = "content_type")
+	private String contentType;
+	
 	@PrePersist @PreUpdate
 	private void prePersistUpdate() {
 		sku = sku.toUpperCase();
 	}
 	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
 	public String getSku() {
 		return sku;
 	}
