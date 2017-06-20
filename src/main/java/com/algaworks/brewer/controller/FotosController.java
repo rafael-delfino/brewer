34fbdcb2c9095370bf377329cf38511f8128c1d5
Rafael Pereira Delfino <rafaelp.delfino@gmail.com>
2017-06-19 21:10:30 -0300

    Mostrando a foto na tela

--- src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -1,6 +1,9 @@
 package com.algaworks.brewer.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,4 +32,8 @@ public class FotosController {
 		return resultado;
 	}
 	
+	@GetMapping("/temp/{nome:.*}")
+	public byte[] recuperarFotoTemporaria(@PathVariable String nome) {
+		return fotoStorage.recuperarFotoTemporaria(nome);
+	}
 }
