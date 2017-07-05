ed8a0f04a11a41d4060552465b98bc1a135f1902
Rafael Pereira Delfino <rafaelp.delfino@gmail.com>
2017-07-04 21:24:55 -0300

    Paginação e Filtro

--- src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -1,12 +1,16 @@
 package com.algaworks.brewer.repository.helper.cerveja;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -18,45 +22,62 @@ public class CervejasImpl implements CervejasQueries {
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@SuppressWarnings({ "unchecked", "deprecation" })
 	@Override
 	@Transactional(readOnly = true)
-	public List<Cerveja> filtrar(CervejaFilter filtro) {
+	public Page<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable) {
+		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
 		
-		CriteriaBuilder cb = manager.getCriteriaBuilder();
-		CriteriaQuery<Cerveja> query = cb.createQuery(Cerveja.class);
-		Root<Cerveja> root = query.from(Cerveja.class);
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
 		
+		criteria.setFirstResult(primeiroRegistro);
+		criteria.setMaxResults(totalRegistrosPorPagina);
+		
+		adicionarFiltro(filtro, criteria);
+		
+		return new PageImpl<>(criteria.list(), pageable, total(filtro));
+	}
+	
+	@SuppressWarnings("deprecation")
+	private Long total(CervejaFilter filtro) {
+		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+		adicionarFiltro(filtro, criteria);
+		criteria.setProjection(Projections.rowCount());
+		return (Long) criteria.uniqueResult();
+	}
+
+	private void adicionarFiltro(CervejaFilter filtro, Criteria criteria) {
 		if (filtro != null) {
 			if (!StringUtils.isEmpty(filtro.getSku())) {
-				query.select(root).where(cb.equal(root.get("sku"), filtro.getSku()));
+				criteria.add(Restrictions.eq("sku", filtro.getSku()));
 			}
 			
 			if (!StringUtils.isEmpty(filtro.getNome())) {
-				query.select(root).where(cb.like(root.get("nome"), "%" + filtro.getNome() + "%"));
+				criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
 			}
-			
+
 			if (isEstiloPresente(filtro)) {
-				query.select(root).where(cb.equal(root.get("estilo"), filtro.getEstilo()));
+				criteria.add(Restrictions.eq("estilo", filtro.getEstilo()));
 			}
 
 			if (filtro.getSabor() != null) {
-				query.select(root).where(cb.equal(root.get("sabor"), filtro.getSabor()));
+				criteria.add(Restrictions.eq("sabor", filtro.getSabor()));
 			}
 
 			if (filtro.getOrigem() != null) {
-				query.select(root).where(cb.equal(root.get("origem"), filtro.getOrigem()));
+				criteria.add(Restrictions.eq("origem", filtro.getOrigem()));
 			}
 
 			if (filtro.getValorDe() != null) {
-				query.select(root).where(cb.ge(root.get("valor"), filtro.getValorDe()));
+				criteria.add(Restrictions.ge("valor", filtro.getValorDe()));
 			}
 
 			if (filtro.getValorAte() != null) {
-				query.select(root).where(cb.le(root.get("valor"), filtro.getValorAte()));
+				criteria.add(Restrictions.le("valor", filtro.getValorAte()));
 			}
 		}
-		
-		return manager.createQuery(query).getResultList();
 	}
 	
 	private boolean isEstiloPresente(CervejaFilter filtro) {
